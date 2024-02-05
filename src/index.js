97abfe7e7370db0934293f05746994d84039d104
krisztinamolnar <krisztina.molnar.mail@gmail.com>
2024-02-06 00:16:45 +0100

    Injected the forecast HTML from JavaScript
    Plus small CSS changes

--- src/index.js
+++ src/index.js
@@ -54,7 +54,35 @@ function handleSearchSubmit(event) {
   searchCity(SearchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` <div class="row">
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img
+              src="http://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="42"
+            />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">18° </span>
+              <span class="weather-forecast-temperature-min">12°</span>
+            </div>
+          </div>
+        </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let SearchFormElement = document.querySelector("#search-form");
 SearchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Vienna");
+displayForecast();
